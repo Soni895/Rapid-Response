6f755f7e4b7fb680db371b3eac4e2042dff470d9
soni895 <darshansoni895@gmail.com>
2023-07-16 11:54:23 +0530

    firebase updated

--- config/Firebase.jsx
+++ config/Firebase.jsx
@@ -7,15 +7,24 @@ import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBOIVay3_mPI3e5qj6m7-o00b2BPBgIaQY",
+//   authDomain: "hackverse-5ecdd.firebaseapp.com",
+//   databaseURL: "https://hackverse-5ecdd-default-rtdb.firebaseio.com",
+//   projectId: "hackverse-5ecdd",
+//   storageBucket: "hackverse-5ecdd.appspot.com",
+//   messagingSenderId: "316801498344",
+//   appId: "1:316801498344:web:f70c7cb41d44f5bcf21d5d",
+//   measurementId: "G-E8W4PT6DS5"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBOIVay3_mPI3e5qj6m7-o00b2BPBgIaQY",
-  authDomain: "hackverse-5ecdd.firebaseapp.com",
-  databaseURL: "https://hackverse-5ecdd-default-rtdb.firebaseio.com",
-  projectId: "hackverse-5ecdd",
-  storageBucket: "hackverse-5ecdd.appspot.com",
-  messagingSenderId: "316801498344",
-  appId: "1:316801498344:web:f70c7cb41d44f5bcf21d5d",
-  measurementId: "G-E8W4PT6DS5"
+  apiKey: "AIzaSyAb043n13F2p1La4F6vbwq3yflY1pCh1xE",
+  authDomain: "rapid-respnse.firebaseapp.com",
+  projectId: "rapid-respnse",
+  storageBucket: "rapid-respnse.appspot.com",
+  messagingSenderId: "933474583852",
+  databaseURL:"https://rapid-respnse-default-rtdb.firebaseio.com",
+  appId: "1:933474583852:web:910cd332ff10195570d889"
 };
 
 const app = initializeApp(firebaseConfig);
